8fe0d7f9e5a3f340cc043839eda3d11e174bf79b
nashahgit <nashah25@gmail.com>
2016-11-16 17:27:21 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,59 @@
+﻿$(document).ready(function(){ 
+
+	/* To set Navbar active */
+	$(".nav a").on("click", function(){
+	   $(".nav").find(".active").removeClass("active");
+	   $(this).parent().addClass("active");
+	});
+
+
+	/* Smooth scroll */
+	$(function() {
+	  $('a[href*="#"]:not([href="#"])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html, body').animate({
+	          scrollTop: target.offset().top
+	        }, 800);
+	        return false;
+	      }
+	    }
+	  });
+	});
+
+	/* code for hiding and revealing the BG for navigation */
+	// window.onscroll = function(ev) {
+	//     if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+	//     	// $('.navbar').fadeOut('slow');
+	//     	$('.navbar-default').css({'background-color' : '#3a91eb'});
+	//     }
+	//     else {
+	//     	$('.navbar-default').css({'background-color' : '#ffffff'});
+	//     }
+	// };
+
+	$(function(){
+		$(window).scroll(function(){
+			// var aTop = $('#header').height();
+			if($(this).scrollTop()>=300){
+				// $(".small-owl").fadeIn('slow');
+				$('.navbar-default').css({'background-color' : '#185a9d'}).fadeIn('slow');
+				$('.navbar-brand img').css({'height' : '75%' , '-webkit-transition' : '0.5s', 'transition' : '0.5s'});
+				$('.navbar').css({'min-height' : '50px' , 'padding' : '0.5%' , '-webkit-transition' : '0.5s' , 'transition' : '0.5s' ,
+				 'box-shadow' : 'rgba(0, 0, 0, 0.74902) 0px 1px 5px 0px'});
+				
+			}
+			else {
+				// $(".small-owl").fadeOut('slow');
+				$('.navbar-default').css({'background' : 'none'});
+				$('.navbar-brand img').css({'height' : '85%'});
+				$('.navbar').css({'min-height' : '100px' , 'padding' : '2%'});
+				$('.navbar').css({'box-shadow' : 'rgba(0, 0, 0, 0.74902) 0px 0px 0px 0px'});
+			}
+		});
+	});
+
+
+});
